142f4baa80db8aaa76e04a942859cc67dfe75bb7
EdW1Nas <91197274+EdW1Nas@users.noreply.github.com>
2023-06-27 10:44:42 +0300

    Add files via upload

--- MusicPlatformConsumer/src/main/java/lt/viko/eif/eulozas/soap/musicplatformconsumer/ConsumingWebServiceApplication.java
+++ MusicPlatformConsumer/src/main/java/lt/viko/eif/eulozas/soap/musicplatformconsumer/ConsumingWebServiceApplication.java
@@ -1,11 +1,9 @@
 package lt.viko.eif.eulozas.soap.musicplatformconsumer;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.example.songs.wsdl.GetSongResponse;
 
 @SpringBootApplication
 public class ConsumingWebServiceApplication {
@@ -14,17 +12,6 @@ public class ConsumingWebServiceApplication {
 		SpringApplication.run(ConsumingWebServiceApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner lookup(SongClient quoteClient) {
-		return args -> {
-			String song = "Cool";
 
-			if (args.length > 0) {
-				song = args[0];
-			}
-			GetSongResponse response = quoteClient.getSong(song);
-			System.err.println(response.getSong().getGenre());
-		};
-	}
 
 }
